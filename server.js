a3bfee39416adbb4be6d7b1b45836428e820ce2e
hil <hil679922@gmail.com>
2023-02-14 00:26:11 +0900

    connect localhost server

---
+++ server.js
@@ -0,0 +1,26 @@
+//기본파일인 server.js
+
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.static('asset'));
+
+app.listen(8000, function(){
+  console.log('Listening at 8000');
+});
+app.get('/',function(req,res){
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/about',function(req,res){
+    res.sendFile(__dirname + '/detailPage/about/about.html');
+});
+app.get('/industry',function(req,res){
+    res.sendFile(__dirname + '/detailPage/industry/industry.html');
+});
+app.get('/manufacture',function(req,res){
+    res.sendFile(__dirname + '/detailPage/product/items.html');
+});
+app.get('/contact',function(req,res){
+    res.sendFile(__dirname + '/detailPage/contact/contact.html');
+});
