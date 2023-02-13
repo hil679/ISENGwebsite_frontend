556addb375059b4d7726b7a65053a66330ba9d27
hil <hil679922@gmail.com>
2023-02-14 00:20:09 +0900

    Remove: not a common elements

--- main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navbar__toogleBtn img');
-const menu = document.querySelector('.navbar__menu');
-const navbar = document.querySelector('.navbar');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active')
-    navbar.classList.toggle('active')
-});
