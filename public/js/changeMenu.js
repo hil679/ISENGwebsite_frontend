598eaf56982b5c3e5e2506526a861a05357d8e3c
hil <hil679922@gmail.com>
2023-02-14 22:10:31 +0900

    Refactor: divide slider machine and item

--- public/js/changeMenu.js
+++ public/js/changeMenu.js
@@ -3,11 +3,15 @@ function changeMenu(value){
         case '0': 
             $('.machine').css("display","block");
             $('.product').css("display","none");
+            $('.representative-product-background').css("visibility","hidden");
+            $('.representative-machine-background').css("visibility","visible");
             break;    
     
         case '1':
             $('.machine').css("display","none");
             $('.product').css("display","block");
+            $('.representative-product-background').css("visibility","visible");
+            $('.representative-machine-background').css("visibility","hidden");
             break;
     }
 };
