a0c5671b198985a74c14796bf6ec80e4351a60b4
hil <hil679922@gmail.com>
2023-02-18 02:48:25 +0900

    Change: cursor shape when hover, Change: table appearing moment when onclick

--- public/js/changeMenu.js
+++ public/js/changeMenu.js
@@ -11,3 +11,64 @@ function changeMenu(value){
             break;
     }
 };
+
+function machineChangeMenu(value){
+    switch(value){
+        case '0': 
+            $('.facility-status').css("display","block");
+            $('.processing-facility').css("display","none");
+            $('.measuring-facility').css("display","none");
+            $('.navbar2-facility-status').css({"border-bottom": "1px solid darkblue"});
+            $('.navbar2-processing-facility').css("border-bottom","0px");
+            $('.navbar2-measuring-facility').css("border-bottom","0px");
+            break;    
+    
+        case '1':
+            $('.facility-status').css("display","none");
+            $('.processing-facility').css("display","block");
+            $('.measuring-facility').css("display","none");
+            $('.navbar2-facility-status').css("border-bottom", "0px");
+            $('.navbar2-processing-facility').css("border-bottom","1px solid darkblue");
+            $('.navbar2-measuring-facility').css("border-bottom","0px");
+            break;
+        case '2':
+            $('.facility-status').css("display","none");
+            $('.processing-facility').css("display","none");
+            $('.measuring-facility').css("display","block");
+            $('.navbar2-facility-status').css("border-bottom", "0px");
+            $('.navbar2-processing-facility').css("border-bottom","0px");
+            $('.navbar2-measuring-facility').css("border-bottom","1px solid darkblue");
+            break;    
+    }
+};
+
+
+function changeMachineSpec(value){
+    switch(value){
+        case '0': 
+            $('.first-machining-center').css("display","block");
+            $('.second-machining-center').css("display","none");
+            $('.third-machining-center').css("display","none");
+            $('.forth-machining-center').css("display","none");
+            break;    
+    
+        case '1':
+            $('.second-machining-center').css("display","block");
+            $('.first-machining-center').css("display","none");
+            $('.third-machining-center').css("display","none");
+            $('.forth-machining-center').css("display","none");
+            break;
+        case '2':
+            $('.third-machining-center').css("display","block");
+            $('.second-machining-center').css("display","none");
+            $('.first-machining-center').css("display","none");
+            $('.forth-machining-center').css("display","none");
+            break;  
+        case '3':
+            $('.forth-machining-center').css("display","block");
+            $('.second-machining-center').css("display","none");
+            $('.third-machining-center').css("display","none");
+            $('.first-machining-center').css("display","none");
+            break;   
+    }
+};
