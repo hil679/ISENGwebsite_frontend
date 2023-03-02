4f88c5e1236396f83738d3d6ffb619ca04d41657
hil <hil679922@gmail.com>
2023-03-02 23:39:30 +0900

    Refactor & Change: code, img, location etc

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
-app.use(express.static('asset'));
+app.use(express.static('./public'));
+app.use(express.static('./asset'));
 
 app.listen(8000, function(){
   console.log('Listening at 8000');
@@ -12,18 +12,18 @@ app.listen(8000, function(){
 app.get('/',function(req,res){
     res.sendFile(__dirname + '/index.html');
 });
-app.get('/abou',function(req,res){
+app.get('/about',function(req,res){
     res.sendFile(__dirname + '/detailPage/about/about.html');
 });
-app.get('/industr',function(req,res){
+app.get('/industry',function(req,res){
     res.sendFile(__dirname + '/detailPage/industry/industry.html');
 });
-app.get('/manufacture',function(req,res){
+app.get('/manufacture/',function(req,res){
     res.sendFile(__dirname + '/detailPage/product/items.html');
 });
-app.get('/machine',function(req,res){
+app.get('/machine/',function(req,res){
     res.sendFile(__dirname + '/detailPage/machine/machine.html');
 });
-app.get('/contact',function(req,res){
+app.get('/contact/',function(req,res){
     res.sendFile(__dirname + '/detailPage/contact/contact.html');
 });
