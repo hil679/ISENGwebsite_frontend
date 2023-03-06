7fa053e42f89cf680df5abacb37fda2f7f10f353
hil <hil679922@gmail.com>
2023-03-06 23:17:57 +0900

    Change: mainslider design

---
+++ public/js/sliderArrow.js
@@ -0,0 +1,10 @@
+$(".swiper-prev").hover(function(){
+    $(".swiper-prev img").css("opacity", "100%");},
+    function(){
+        $(".swiper-prev img").css("opacity", "40%");
+})
+$(".swiper-next").hover(function(){
+    $(".swiper-next img").css("opacity", "100%");},
+    function(){
+        $(".swiper-next img").css("opacity", "40%");
+})
