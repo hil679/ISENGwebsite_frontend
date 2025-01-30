c74300759a1e7daa63efb083bc8c1f3d3029d223
hil679 <hil679922@gmail.com>
2025-01-31 00:01:27 +0900

    style: remove head part because of changing code, fix navbar js script

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,11 +1,19 @@
-$(document).ready(function(){
-    $('nav > .container-fluid > .hover-sub > ul > li').mouseover(function(){
-        $(this).children('.dropdown-menu').stop().fadeIn(400);
+$(document).on('mouseover', '.navbar > .container-fluid > .hover-sub > ul > li', function () {
+    $(this).children('.dropdown-menu').stop().fadeIn(400);
 });
-$('nav > .container-fluid > .hover-sub > ul > li').mouseleave(function(){
+
+$(document).on('mouseleave', '.navbar > .container-fluid > .hover-sub > ul > li', function () {
     $(this).children('.dropdown-menu').stop().fadeOut();
-    });
 });
+
+// $(document).ready(function(){
+//     $('.navbar > .container-fluid > .hover-sub > ul > li').mouseover(function(){
+//         $(this).children('.dropdown-menu').stop().fadeIn(400);
+// });
+// $('nav > .container-fluid > .hover-sub > ul > li').mouseleave(function(){
+//     $(this).children('.dropdown-menu').stop().fadeOut();
+//     });
+// });
 
 mobileNavToggle = function () {
     const $sidebar = $("#sidebar");
